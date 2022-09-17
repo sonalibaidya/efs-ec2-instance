e0ad033ab797434b843e851cfd9389f557d5f37f
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-17 17:06:27 +0530

    Update aws_instance.tf

--- aws_instance.tf
+++ aws_instance.tf
@@ -25,19 +25,3 @@ resource "aws_instance" "testinstance" {
     Name = "testinstance"
   }
 }
-
-resource "aws_instance" "web" {
-  connection {
-    type     = "ssh"
-    user     = "root"
-    password = var.root_password
-    host     = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      # Mounting Efs 
-      "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.efs.dns_name}:/  /var/www/html",
-    ]
-  }
-}
