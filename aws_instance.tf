406eea80a99937982de6addf3a1ac319ac769dba
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-17 15:41:47 +0530

    Update aws_instance.tf

--- aws_instance.tf
+++ aws_instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "testinstance" {
-  ami                         = "ami-087c17d1fe0178315"
+  ami                         = "ami-05fa00d4c63e32376"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet[0].id
   associate_public_ip_address = true
