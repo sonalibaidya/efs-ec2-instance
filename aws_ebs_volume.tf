bcfa3fb8d4ee70a205f9dc276aee2e3a9b3741a7
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-17 17:10:03 +0530

    Update aws_ebs_volume.tf

--- aws_ebs_volume.tf
+++ aws_ebs_volume.tf
@@ -19,18 +19,21 @@ resource "aws_efs_mount_target" "efs-mt" {
 
 
 # Creating Mount Point for EFS
-
-resource "aws_instance" "web" {
+resource "null_resource" "configure_nfs" {
   depends_on = [aws_efs_mount_target.efs-mt]
   connection {
     type        = "ssh"
     user        = "ec2-user"
     private_key = tls_private_key.my_key.private_key_pem
-    host        = aws_instance.testinstance.public_ip
+    host        = aws_instance.web.public_ip
   }
 
   provisioner "remote-exec" {
     inline = [
+      "sudo yum install httpd php git -y -q ",
+      "sudo systemctl start httpd",
+      "sudo systemctl enable httpd",
+      "sudo yum install nfs-utils -y -q ", # Amazon ami has pre installed nfs utils
       # Mounting Efs 
       "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.efs.dns_name}:/  /var/www/html",
     ]
