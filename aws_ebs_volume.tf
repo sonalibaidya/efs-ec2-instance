3c8bc1f0b2857ade3782a3f9ef6b2d51c08e9ad6
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-17 17:04:37 +0530

    xxxxxxxxxxxxxx

--- aws_ebs_volume.tf
+++ aws_ebs_volume.tf
@@ -19,12 +19,20 @@ resource "aws_efs_mount_target" "efs-mt" {
 
 
 # Creating Mount Point for EFS
-resource "null_resource" "configure_nfs" {
+
+resource "aws_instance" "web" {
   depends_on = [aws_efs_mount_target.efs-mt]
   connection {
     type        = "ssh"
     user        = "ec2-user"
     private_key = tls_private_key.my_key.private_key_pem
     host        = aws_instance.testinstance.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # Mounting Efs 
+      "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.efs.dns_name}:/  /var/www/html",
+    ]
   }
 }
