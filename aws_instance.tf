b63ce1fded52d116880e3c5f7e83193efdfc0a6a
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-17 19:09:38 +0530

    Update aws_instance.tf

--- aws_instance.tf
+++ aws_instance.tf
@@ -1,26 +1,10 @@
-# Generate new private key
-resource "tls_private_key" "my_key" {
-  algorithm = "RSA"
-}
-# Generate a key-pair with above key
-resource "aws_key_pair" "deployer" {
-  key_name   = "efs-key"
-  public_key = tls_private_key.my_key.public_key_openssh
-}
-# Saving Key Pair for ssh login for Client if needed
-resource "null_resource" "save_key_pair" {
-  provisioner "local-exec" {
-    command = "echo  ${tls_private_key.my_key.private_key_pem} > mykey.pem"
-  }
-}
-
 resource "aws_instance" "testinstance" {
   ami                         = "ami-05fa00d4c63e32376"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet[0].id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.ec2.id]
-  key_name                    = aws_key_pair.deployer.key_name
+  key_name                    = "new-eks"
   tags = {
     Name = "testinstance"
   }
