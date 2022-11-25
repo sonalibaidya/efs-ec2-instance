b154a784f6fdd217c7dc3ec36990d4c0ba9112ea
sonalibaidya <102017391+sonalibaidya@users.noreply.github.com>
2022-11-24 22:44:51 -0800

    Update aws_instance.tf

--- aws_instance.tf
+++ aws_instance.tf
@@ -1,7 +1,8 @@
 module "key_pair" {
   source     = "terraform-aws-modules/key-pair/aws"
   key_name   = "deployer-three"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0TLsSPoiOSnac9MqVcaYh+NlJ0cqS4AKlNA9dh5CliG/jyaH1zUH6y4lWlqPBRBQ610UpKMST48cbbRVHYlZ7Suf415/Jp1zjM7NUS7H4USdbLI0qFL7OTHUSyyQBYG565EFsTMdUknVikQM6T2B1Dbs46JoRubfNlKg94YQljNl7inBFiks+/DKxXpcq/p1znvUz/cdWP5C77tXx8RB0vpFLDTSw0jRc/legZ/VdcqSpZJFOION5F+7HkiM5YT3QYFdNuA9khCL+Iqwd2LgcHWF12nFn+TQXvt9yl4VyXeLqc9GQeknGartTRcGc+gyW+3iDNjhdTcjHmBJwlMMSIAbEKW3QT1n3PkiCgH37/Df3GVtGCPVKmo1jnyFHkotSjqnz6ixw7H1IXyOYq2ED9BtLScyc8Za0j+p+D5M5qTGeWaBoiwg1gnXr6QuTzegi/ei2GYSrnNe0vmBEtMj+ctViXvV3TBn783lg+nNafOx00/T1snSH5Q2BvUESDB0= root@ip-172-31-85-18"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJQoC4MRv9Ng/80P9Q6ZybFSztnETaoywaOYJnF8HEv5fOGoNbFoIeOQq3P2PkH14xh/wmMiMb3BAQsbpvRvXO9Zg4u3kjyJF/r251ip75w2p7pm1Rp6CW2L7k6BrJOrxQf8knzzyK1yyGo6WV5cEvxq16rEf5vFwF/0PgeprXPdWhwVSbxu4gDW3KSOHiJr648ZxP8T9rmzYrsAqE7TaQ8h7mcRsegONn3zUTNCB5UHKllDirwDXoA6x3lhGRy9jSmWh+B+KSHK6+TlsxEn3aqKl2h05M3+eG0gCNwWiRCLBKVzxP+Sq9nu3IvnIeHQ0f/9+hEjs0P0ZQKvkddni1 root@ip-172-31-42-1
+"
 }
 
 
