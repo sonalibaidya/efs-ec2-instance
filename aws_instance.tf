d11fc349b740dfc2a66545563b3a10a444743a7d
sonalibaidya <102017391+sonalibaidya@users.noreply.github.com>
2022-11-25 03:37:21 -0800

    Update aws_instance.tf

--- aws_instance.tf
+++ aws_instance.tf
@@ -1,6 +1,6 @@
 module "key_pair" {
   source     = "terraform-aws-modules/key-pair/aws"
-  key_name   = "aswin"
+  key_name   = "newkey"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJQoC4MRv9Ng/80P9Q6ZybFSztnETaoywaOYJnF8HEv5fOGoNbFoIeOQq3P2PkH14xh/wmMiMb3BAQsbpvRvXO9Zg4u3kjyJF/r251ip75w2p7pm1Rp6CW2L7k6BrJOrxQf8knzzyK1yyGo6WV5cEvxq16rEf5vFwF/0PgeprXPdWhwVSbxu4gDW3KSOHiJr648ZxP8T9rmzYrsAqE7TaQ8h7mcRsegONn3zUTNCB5UHKllDirwDXoA6x3lhGRy9jSmWh+B+KSHK6+TlsxEn3aqKl2h05M3+eG0gCNwWiRCLBKVzxP+Sq9nu3IvnIeHQ0f/9+hEjs0P0ZQKvkddni1 root@ip-172-31-42-1"
 }
 
@@ -11,7 +11,7 @@ resource "aws_instance" "testinstance" {
   subnet_id                   = aws_subnet.subnet[0].id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.ec2.id]
-  key_name                    = "aswin"
+  key_name                    = "newkey"
   tags = {
     Name = "testinstance"
   }
