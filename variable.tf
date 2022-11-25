7480705930ba127de0513b4264401e1398653a5a
sonalibaidya <102017391+sonalibaidya@users.noreply.github.com>
2022-11-25 02:16:47 -0800

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,5 +7,5 @@ variable "public_key" {
 variable "private_key" {
   type        = string
   description = "File path of private key."
-  default     = "~/.ssh/id_rsa.prv"
+  default     = "~/.ssh/id_rsa"
 }
