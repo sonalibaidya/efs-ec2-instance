0b7e7257ed73d86046bc6830fa6ca990d6574089
sonalibaidya <102017391+sonalibaidya@users.noreply.github.com>
2022-11-28 22:10:19 -0800

    Update aws_efs_volume.tf

--- aws_efs_volume.tf
+++ aws_efs_volume.tf
@@ -19,7 +19,7 @@ resource "aws_efs_mount_target" "efs-mt" {
 
 
 # Creating Mount Point for EFS
-resource "Efs" "configure_nfs" {
+resource "aws_EFS" "configure_nfs" {
   depends_on = [aws_efs_mount_target.efs-mt]
   connection {
     type        = "ssh"
